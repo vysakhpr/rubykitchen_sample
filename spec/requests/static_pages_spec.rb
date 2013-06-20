7afd2862a018ff186df05cbd26ce98d176970a9c
vysakhpr <my_mail_is_buzy@yahoo.com.au>
2013-06-20 14:38:52 +0530

    Finish User Sign Up

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,6 +4,14 @@ describe "Static pages" do
 
   subject { page }
 
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
+
   describe "Home page" do
     before { visit root_path }
 
