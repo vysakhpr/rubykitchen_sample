0283e3900d3112a262f5d32cdc1b30ba0007595b
vysakhpr <my_mail_is_buzy@yahoo.com.au>
2013-06-21 16:14:52 +0530

    10.1.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
-
+  has_many :microposts
+  
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
