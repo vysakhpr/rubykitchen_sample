ed5d7fdfcf5a34d99a9c275afc73ddba6e7c657b
vysakhpr <my_mail_is_buzy@yahoo.com.au>
2013-06-25 13:10:43 +0530

    11.3.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,8 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
   after_validation { self.errors.messages.delete(:password_digest) }
 
-  def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ?", id)
+ def feed
+    Micropost.from_users_followed_by(self)
   end
 
   def following?(other_user)
